e303204615b04fc5e1504b11abcea401f42f2380
Dheeraj <dheerajsingh9933@gmail.com>
2026-04-01 08:27:55 +0530

    removing node modules

--- build/desktop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dheeraj/Code/actual project /teleport/Teleport-v1/desktop
-# Build directory: /home/dheeraj/Code/actual project /teleport/Teleport-v1/build/desktop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- release/Teleport/README.txt
+++
@@ -1,4 +0,0 @@
-Teleport - Fast LAN File Transfer
-
-Run Teleport.exe for GUI
-Run Teleport_CLI.bat for CLI
